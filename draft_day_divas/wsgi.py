18d27a5ea60b7f985992335c8f63a6c57880f737
rsvagle <rsvagle@gmail.com>
2024-03-30 19:06:10 -0500

    minor

--- draft_day_divas/wsgi.py
+++ draft_day_divas/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'draft_day_divas.settings')
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
