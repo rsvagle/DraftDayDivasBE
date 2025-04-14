e0244b8f6d1882d308748ff93e58030685c18f77
rsvagle <rsvagle@gmail.com>
2025-04-13 19:50:50 -0500

    progress

--- api/consumers.py
+++ api/consumers.py
@@ -1,5 +1,6 @@
 import os
 import django
+import time
 
 # Set up Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'draft_day_divas.settings')
@@ -10,9 +11,13 @@ from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import DenyConnection
 from channels.db import database_sync_to_async
+from django.shortcuts import get_object_or_404
+from .models import FantasyDraft
+from .serializers import FantasyDraftSerializer
 import json
 
 class DraftConsumer(AsyncWebsocketConsumer):
+    # connect a user
     async def connect(self):
         try:
             token_key = self.scope['query_string'].decode().split('token=')[1] # or from headers
@@ -20,17 +25,15 @@ class DraftConsumer(AsyncWebsocketConsumer):
             user = token.user
             self.scope['user'] = user
         except Token.DoesNotExist:
-            print("Token not found")
             raise DenyConnection("Invalid token")
         except Exception as e:
-            print(f"Authentication failure: {e}")
             raise DenyConnection("Authentication failed")
 
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"draft_{self.room_name}"
 
         # Ensure user has permission to join this room
-        if not await self.user_can_join():
+        if not await self.user_can_join(self.room_name):
             await self.close()
             return
 
@@ -41,12 +44,36 @@ class DraftConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Wait a bit then announce the user has joined
+        time.sleep(2)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "username": "System",
+                "message": user.username + " joined the draft!",
+            }
+        )
+
+    # Disconnect a user
     async def disconnect(self, close_code):
+        user = self.scope['user']
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        # Wait a bit then announce the user has left
+        time.sleep(2)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "username": "System",
+                "message": user.username + " has left the draft!",
+            }
+        )
 
+    # Receive user input
     async def receive(self, text_data):
         data = json.loads(text_data)
 
@@ -70,14 +97,10 @@ class DraftConsumer(AsyncWebsocketConsumer):
             elif message_type == 'draft_pick':
                 await self.make_draft_selection(text_data)
 
-    async def user_can_join(self):
-        # """
-        # Check if the authenticated user is allowed to join the draft room.
-        # """
-        # Implement your logic here (e.g., checking if they are part of the draft)
-        return True  # Replace with actual permission checks
-
+    # 
     # Methods
+    #
+
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
             "type": event["type"],
@@ -85,19 +108,29 @@ class DraftConsumer(AsyncWebsocketConsumer):
             "text": event["message"],
         }))
 
+    
     async def send_lobby_state(self):
         # Send the updated lobby state """
-        lobby_state = await self.get_lobby_state()
+        lobby_state = await self.get_lobby_state(self.room_name)
         await self.send(text_data=json.dumps({
-            "type": "lobby_state",
+            "type": "draft_state",
             "state": lobby_state
     }))
 
-    async def get_lobby_state(self):
+    @database_sync_to_async
+    def get_lobby_state(self, draft_id):
         # Fetch lobby state (mocked for now, ideally from DB/cache) """
-        return {"players": ["user1", "user2"], "status": "active"}
+        draft_instance = get_object_or_404(FantasyDraft, id=draft_id)
+        serializer = FantasyDraftSerializer(draft_instance)
+        return serializer.data
     
     async def make_draft_selection(self, text_data):
+        # check it's the user's turn
+
+        # check that this player hasn't been selected yet
+
+        # pick the player
+
         data = json.loads(text_data)
         username = data['username']
         player_name = data['player']['player']
@@ -107,10 +140,24 @@ class DraftConsumer(AsyncWebsocketConsumer):
             {
                 "type": "chat_message",
                 "username": username,
-                "message": 'Drafted:' + player_name + 'at pick ' + str(pick),
+                "message": 'Drafted:' + player_name + ' at pick ' + str(pick),
             }
         )
 
+    # 
+    # Auth helpers
+    # 
     @database_sync_to_async
     def get_token(self, token_key):
         return Token.objects.select_related('user').get(key=token_key)
+
+    @database_sync_to_async
+    def user_can_join(self, draft_id):
+        draft_instance = get_object_or_404(FantasyDraft, id=draft_id)
+        user = self.scope["user"]
+
+        # Check if any draft_team has this user
+        if draft_instance.draft_teams.filter(user=user).exists():
+            return True
+
+        return False
